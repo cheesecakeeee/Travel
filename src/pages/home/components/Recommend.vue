6c6ba89a1044cd1fd065016c089f181c210d0622
zhengcy <zhengcy@cloudream.com>
2018-09-27 12:04:47 +0800

    add remommend&weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item" v-for="item in recommendList" :key="item.id">
+
+          <img :src="item.imgUrl" class="item-img" alt="">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeRecommend',
+    data(){
+      return {
+        recommendList:[
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_200x200_4ab5da75.jpg',
+            title:'大连圣亚海洋世界',
+            desc:'哈哈哈哈哈哈哈哈哈哈或或或或哈哈哈哈哈哈哈哈哈哈'
+          },
+          {
+            id:'0002',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1707/57/5729585581b6732fa3.water.jpg_200x200_85298361.jpg',
+            title:'大连森林动物园',
+            desc:'哈哈哈哈哈哈哈哈哈哈或或或或哈哈哈哈哈哈哈哈哈哈'
+          },
+          {
+            id:'0003',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1602/39/39f6b1003abe1b2790.img.jpg_200x200_a7051b04.jpg',
+            title:'老虎滩海洋公园',
+            desc:'哈哈哈哈哈哈哈哈哈哈或或或或哈哈哈哈哈哈哈哈哈哈'
+          },
+          {
+            id:'0004',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1808/2f/2fc9a5055a1b287ca3.water.jpg_200x200_4ea76958.jpg',
+            title:'海昌发现王国',
+            desc:'哈哈哈哈哈哈哈哈哈哈或或或或哈哈哈哈哈哈哈哈哈哈'
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "~Styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent:.2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      float: 1;
+      min-width:0
+      padding: .1rem
+      .item-title
+        line-height: .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .1rem
+        background: #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        color: #fff
+
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item" v-for="item in recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" class="item-img" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeWeekend',
+    data(){
+      return {
+        recommendList:[
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1505/b7/8a08a1023062b3.jpg_r_640x214_d7cb475c.jpg',
+            title:'最赞的海滨浴场',
+            desc:'哈哈哈哈哈哈哈哈哈哈或或或或哈哈哈哈哈哈哈哈哈哈'
+          },
+          {
+            id:'0002',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1505/7f/fa4eb0ca72409d.jpg_r_640x214_2f2a860e.jpg',
+            title:'大连必游TOP10',
+            desc:'哈哈哈哈哈哈哈哈哈哈或或或或哈哈哈哈哈哈哈哈哈哈'
+          },
+          {
+            id:'0003',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1507/6b/70d17d828d87e1.jpg_r_640x214_6a995270.jpg',
+            title:'周末游首选',
+            desc:'哈哈哈哈哈哈哈哈哈哈或或或或哈哈哈哈哈哈哈哈哈哈'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "~Styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent:.2rem
+  .item-img-wrapper
+    height: 0
+    overflow: hidden
+    padding-bottom:33.9%
+    .item-img
+      width: 100%
+  .item-title
+    line-height: .54rem
+    font-size .32rem
+    ellipsis()
+  .item-desc
+    line-height: .4rem
+    color: #ccc
+    ellipsis()
+
+
+</style>
