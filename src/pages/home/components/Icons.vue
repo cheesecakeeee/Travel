974ad9d1548dece157352cb087c55501c6c548ba
zhengcy <zhengcy@cloudream.com>
2018-09-27 10:44:44 +0800

    add icon swiper

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,55 +1,15 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-      </div>
-      <p class="icon-desc">景点门票</p>
-    </div>
-
-
+    <swiper>
+      <swiper-slide v-for="(page,index) in pages" :key = 'index'>
+        <div class="icon" v-for="item in page" :key="item.key">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
@@ -58,7 +18,66 @@
     name: 'HomeIcons',
     data(){
       return {
-
+        iconList:[
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc:'景点门票哈哈哈哈'
+          },
+          {
+            id:'0002',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+            desc:'水上乐园'
+          },
+          {
+            id:'0003',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+            desc:'游乐场'
+          },
+          {
+            id:'0004',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+            desc:'海洋馆'
+          },
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc:'景点门票'
+          },
+          {
+            id:'0002',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+            desc:'水上乐园'
+          },
+          {
+            id:'0003',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+            desc:'游乐场'
+          },
+          {
+            id:'0004',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+            desc:'海洋馆'
+          },
+          {
+            id:'0004',
+            imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+            desc:'海洋馆'
+          }
+        ]
+      }
+    },
+    computed:{
+      pages(){
+        const pages = [];
+        this.iconList.forEach((item,index)=>{
+          const page  = Math.floor(index/8);
+          if(!pages[page]){
+            pages[page] = []
+          }
+          pages[page].push(item);
+        })
+        return pages
       }
     }
   }
@@ -66,38 +85,38 @@
 
 <style scoped lang="stylus">
   @import "~Styles/varibles.styl"
-  .icons
-    /*width: 100%*/
-    height: 0
+  @import "~Styles/mixins.styl"
+  .icons >>> .swiper-container
+    height:0
+    padding-bottom:50%
+  .icon
     overflow: hidden
-    padding-bottom: 50%
-    .icon
-      overflow: hidden
-      position: relative
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        right: 0
-        left:0
-        bottom: .44rem
-        box-sizing:border-box
-        padding .1rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height:.44rem
-        line-height: .44rem
-        text-align: center
-        color: $darkTextColor
+    position: relative
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      right: 0
+      left:0
+      bottom: .44rem
+      box-sizing:border-box
+      padding .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height:.44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
 
 </style>
