cf8d1d6d454657aa38463f15b4c14fd24c928336
zhengcy <zhengcy@cloudream.com>
2018-09-28 17:19:53 +0800

    city search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CitySearch'
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "~Styles/varibles.styl"
+  .search
+    height: .72rem
+    padding:0 .1rem
+    background:$bgColor
+    .search-input
+      width: 100%
+      height: .62rem
+      line-height: .62rem
+      box-sizing:border-box
+      padding 0 .1rem
+      text-align: center
+      color: #666
+      border-radius:.06rem
+</style>
