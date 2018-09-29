a5fccd6a5447fa5f4394b31b1ccd52970636b3d0
zhengcy <zhengcy@cloudream.com>
2018-09-29 11:00:02 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,19 +2,42 @@
   <div>
     <city-header/>
     <city-search/>
-    <city-list/>
-    <city-alphabet/>
+    <city-list :hotCities = 'hotCities' :cities = 'cities'/>
+    <city-alphabet :cities = 'cities'/>
   </div>
 </template>
 
 <script>
-
+  import axios from 'axios'
   import CityHeader from './components/Header'
   import CitySearch from './components/Search'
   import CityList from './components/List'
   import CityAlphabet from './components/Alphabet'
   export default {
     name: 'City',
+    methods:{
+      getCityInfo(){
+        axios.get('/api/city.json')
+          .then(this.handleGetCityInfoSucc)
+      },
+      handleGetCityInfoSucc(res){
+        res = res.data
+        if(res.ret && res.data){
+          const  data = res.data
+          this.hotCities = data.hotCities
+          this.cities = data.cities
+        }
+      }
+    },
+    data(){
+      return {
+        hotCities:[],
+        cities:{}
+      }
+    },
+    mounted(){
+      this.getCityInfo()
+    },
     components:{
       CityHeader,
       CitySearch,
