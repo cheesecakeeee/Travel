2ea77408ec0f37c37dae43ae04b5e11aed3c7c80
zhengcy <zhengcy@cloudream.com>
2018-09-29 10:08:55 +0800

    finish city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,20 +3,23 @@
     <city-header/>
     <city-search/>
     <city-list/>
+    <city-alphabet/>
   </div>
 </template>
 
 <script>
+
   import CityHeader from './components/Header'
   import CitySearch from './components/Search'
   import CityList from './components/List'
-
+  import CityAlphabet from './components/Alphabet'
   export default {
     name: 'City',
     components:{
       CityHeader,
       CitySearch,
-      CityList
+      CityList,
+      CityAlphabet
     }
   }
 </script>
