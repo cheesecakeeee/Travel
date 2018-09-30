0a0d886fec24b3c4ba835efd40d551f4f959cf84
zhengcy <zhengcy@cloudream.com>
2018-09-30 16:44:39 +0800

    finish city keepalive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -16,13 +16,14 @@
   import HomeWeekend from './components/Weekend'
   import axios from  'axios'
 
-
+  import { mapState } from 'vuex'
 
   export default {
     name: 'home',
     data(){
       return {
         // city:'',
+        lastCity:"",
         swiperList:[],
         iconList:[],
         recommendList:[],
@@ -36,16 +37,19 @@
       HomeRecommend,
       HomeWeekend
     },
+    computed:{
+      ...mapState(['city'])
+    },
     methods:{
       getHomeInfo(){
-        axios.get('/api/index.json')
+        axios.get('/api/index.json?city='+ this.city)
           .then(this.getHomeInfoSucc)
       },
       getHomeInfoSucc(res){
         res = res.data
         if(res.ret && res.data){
           const data = res.data
-          this.city = data.city
+          // this.city = data.city
           this.swiperList = data.swiperList
           this.iconList = data.iconList
           this.recommendList = data.recommendList
@@ -55,8 +59,16 @@
       }
     },
     mounted(){    //页面挂载el后执行getHomeInfo方法
+      this.lastCity = this.city
       this.getHomeInfo()
+    },
+    activated(){
+      if(this.lastCity !== this.city){
+        this.lastCity = this.city
+        this.getHomeInfo()
+      }
     }
+
 
   }
 </script>
